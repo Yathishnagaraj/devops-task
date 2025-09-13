0e9b28e9c4699098399a19e123be8987ebaa3aee
Yathishnagaraj <91874668+Yathishnagaraj@users.noreply.github.com>
2025-09-13 16:44:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 3050;
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'logoswayatt.png'));
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server running on http://localhost:3000');
 });
